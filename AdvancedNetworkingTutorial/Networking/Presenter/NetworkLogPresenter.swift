6adbcc8f5a1fe1e027eb4b838d9fa8b75fb8d3e2
Sachin Randive <r.sachin81@gmail.com>
2026-06-08 08:39:22 +0530

    Added NetworkLogPresenter class.

---
+++ AdvancedNetworkingTutorial/Networking/Presenter/NetworkLogPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkLogPresenter.swift
+//  AdvancedNetworkingTutorial
+//
+//  Created by Sachin Randive on 08/06/26.
+//
+
+import Foundation
+
+@Observable @MainActor
+final class NetworkLogPresenter {
+    
+}
